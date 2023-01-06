7b906f0381c0b2a2ba971b50574e5c100e5f248e
magnushelliesen <magnus.helliesen@gmail.com>
2023-01-06 09:12:50 +0100

    Changed some names

--- flatten_list.py
+++ flatten_list.py
@@ -1,15 +1,15 @@
 # Recursive function that flattens arbitrarily nested in_list
-def flatten_list(in_list):
-    if isinstance(in_list, list) is False:
+def flatten_list(input_list):
+    if isinstance(input_list, list) is False:
         raise ValueError('Input must be list')
 
     outer_list = []
-    for x in in_list:
+    for element in input_list:
         inner_list = []
-        if isinstance(x, list):
-            inner_list.extend(flatten_list(x))
+        if isinstance(element, list):
+            inner_list.extend(flatten_list(element))
         else:
-            inner_list.append(x)
+            inner_list.append(element)
         outer_list.extend(inner_list)
 
     return outer_list
