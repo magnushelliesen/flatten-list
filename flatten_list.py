ac6823c44c87f311ca6eeafd9123daa5a0eb3187
magnushelliesen <magnus.helliesen@gmail.com>
2023-01-06 09:22:37 +0100

    Made function to take tuples

--- flatten_list.py
+++ flatten_list.py
@@ -1,12 +1,12 @@
 # Recursive function that flattens arbitrarily nested input_list
 def flatten_list(input_list):
-    if isinstance(input_list, list) is False:
+    if (isinstance(input_list, list) or isinstance(input_list, tuple)) is False:
         raise ValueError('Input must be list')
 
     outer_list = []
     for element in input_list:
         inner_list = []
-        if isinstance(element, list):
+        if isinstance(element, list) or isinstance(element, tuple):
             inner_list.extend(flatten_list(element))
         else:
             inner_list.append(element)
